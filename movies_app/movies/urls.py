f4d17f2927380197af6b167ecf81fb6dcc8faf0b
Panagiotis Bellias <belliaspanagiotis@gmail.com>
2021-07-24 21:47:41 +0300

    rate movies use case done

--- movies_app/movies/urls.py
+++ movies_app/movies/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     #path('', views.index, name='home'),
     path('movies/', views.index, name='index'),
     path('movies/<int:movie_id>/', views.detail, name='detail'),
+    path('movies/<int:movie_id>/rate/', views.rate, name='rate'),
 ]
--- movies_app/movies/views.py
+++ movies_app/movies/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import get_object_or_404, render
+from django.http import Http404, HttpResponseRedirect
+from django.urls import reverse
 from .models import Movies
 
 def index(request):
@@ -9,3 +11,14 @@ def index(request):
 def detail(request, movie_id):
     movie = get_object_or_404(Movies, pk=movie_id)
     return render(request, 'movies/detail.html', {'movie': movie})
+
+def rate(request, movie_id):
+    movie = get_object_or_404(Movies, pk=movie_id)
+    
+    try:
+        selected_rating = request.POST['rating']
+    except MultiValueDictKeyError:
+        raise Http404('')
+    movie.rating = round((float(movie.rating) + float(selected_rating)) / 2, 1)
+    movie.save()
+    return HttpResponseRedirect(reverse('index'))
